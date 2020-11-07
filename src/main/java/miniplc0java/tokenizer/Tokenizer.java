543464125078def543e6a771f76b5d3348547fb8
Quasimoodo <63183315+Quasimoodo@users.noreply.github.com>
2020-11-07 15:51:57 +0800

    Revert "Update Tokenizer.java"
    This reverts commit dd0307d2a41b4cb94e95ee53502476f08b665ac5.

--- src/main/java/miniplc0java/tokenizer/Tokenizer.java
+++ src/main/java/miniplc0java/tokenizer/Tokenizer.java
@@ -84,20 +84,20 @@ public class Tokenizer {
         {
             now+=it.nextChar();
         }
-        if(now.equals("begin"))
+        if(now.equalsIgnoreCase("Begin"))
         {
             return new Token(TokenType.Begin,now,beginpos,it.currentPos());
 
         }
-        else if(now.equals("end"))
+        else if(now.equalsIgnoreCase("End"))
         {
             return new Token(TokenType.End,now,beginpos,it.currentPos());
         }
-        else if(now.equals("var"))
+        else if(now.equalsIgnoreCase("Var"))
         {
             return new Token(TokenType.Var,now,beginpos,it.currentPos());
         }
-        else if(now.equals("const"))
+        else if(now.equalsIgnoreCase("Const"))
         {
             return new Token(TokenType.Const,now,beginpos,it.currentPos());
         }
